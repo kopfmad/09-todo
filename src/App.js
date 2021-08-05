cab076e9620bc3aa740b2a1824713d705a52ff7f
andresantana <santana.agr@gmail.com>
2021-08-05 14:12:28 -0300

    consigo adicionar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { useState } from "react";
+
 function App() {
+  let [taskInput, setTaskInput] = useState("");
+  let [taskList, setTaskList] = useState([]);
+
+  function inputHandler(e) {
+    setTaskInput(e.target.value);
+  }
+
+  function addTask() {
+    if (taskInput.length > 0) {
+      setTaskList([...taskList, taskInput]);
+      setTaskInput("");
+    }
+  }
+
   return (
     <div className="App">
       <div className="container">
         <h1>ToDo List</h1>
         <div className="adiciona">
-          <input type="text" placeholder="What do you need to do next?" />
-          <button className="add">ADD</button>
+          <input
+            type="text"
+            placeholder="What do you need to do next?"
+            onChange={(e) => inputHandler(e)}
+            value={taskInput}
+          />
+          <button className="add" onClick={addTask}>
+            ADD
+          </button>
+          {taskList.map((task) => {
+            return (
+              <div className="taskout">
+                <span>{task}</span>
+                <i class="far fa-trash-alt"></i>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
